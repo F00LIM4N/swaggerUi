6843d64d5d78e1a2648e906767edd0a1ecdacfaf
F00LIM4N <leoaznargarrido@gmail.com>
2024-06-07 10:55:08 +0200

    blblbbl

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,20 +1,14 @@
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+  // Begin Swagger UI call region
+  const ui = SwaggerUIBundle({
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
-    deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
     layout: "StandaloneLayout"
-  });
-
-  //</editor-fold>
-};
+  })
+  // End Swagger UI call region
+  window.ui = ui
+}
